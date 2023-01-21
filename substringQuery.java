e5f42bf5dc84d10c1a96971fe422cbe4ad7aebf7
bfenlon <sc0uter498@gmail.com>
2023-01-21 10:49:08 +0100

    substring query start

---
+++ substringQuery.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class anothersubstringqueryproblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLong()) {
+            
+        }
+    }
+}
